e9c5911b101f64c253d996f445a5ee5fafe3dbd5
Meihui Liu <meliu@SZEC02WM1YLHTD7.sea.corp.expecn.com>
2019-12-22 20:04:23 +0800

    add stroybook

---
+++ .storybook/button.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '../src/Button';
+
+export const ButtonStory = () => {
+    return <Button />;
+}
+
+export default {
+    title: 'Button'
+}
---
+++ src/Button.jsx
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react';
+
+class Button extends PureComponent {
+    render() {
+        return (
+            <div>
+                This is a button
+            </div>
+        );
+    }
+}
+
+Button.propTypes = {
+
+};
+
+export default Button;
